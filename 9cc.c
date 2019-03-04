359f931f713f073cf80bdd64a73d7367f37c7894
test <test@test.com>
2019-03-04 14:59:23 +0900

    add ability of plus and minus

--- 9cc.c
+++ 9cc.c
@@ -1,12 +1,12 @@
 /*
  * =====================================================================================
  *
- *       Filename:  test4.c
+ *       Filename:  9cc.c
  *
- *    Description:  pu
+ *    Description:  
  *
  *        Version:  1.0
- *        Created:  03/04/2019 11:49:12 AM
+ *        Created:  03/04/2019 02:35:32 PM
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -20,14 +20,34 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Error: argumetns wrong\n");
+    fprintf(stderr, "Error: argument is wrong\n");
     return 1;
   }
+
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "Error: temrinalted character '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
